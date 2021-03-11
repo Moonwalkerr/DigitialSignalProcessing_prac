d3f63622fb3d7027380a310329471eb7632f8870
Moonwalker <abhisk.mhra@gmail.com>
2021-03-11 20:30:52 +0530

    wip: signal mean algoriithm

--- scripts/Signal Stats and noise/signalmean.py
+++ scripts/Signal Stats and noise/signalmean.py
@@ -1,1 +1,14 @@
-import numpy as np
+# Signal mean algorithm from scratch
+import mysignals as sig
+
+_mean = 0.0
+
+def calculate_signal_mean(signal_arr):
+    global _mean
+    for i in range(len(signal_arr)):
+        _mean += signal_arr[i]
+    _mean = _mean /len(signal_arr)
+    return _mean
+
+
+mean_sig = calculate_signal_mean(sig.)
